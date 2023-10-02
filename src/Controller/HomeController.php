46b45c96e5ba5d429d8757a977e0851efec2c172
ZaraTiana3 <andrianornella@gmail.com>
2023-10-02 12:16:23 +0300

    algo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Service\Algo;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -8,9 +9,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $algo;
+    
+    public function __construct(Algo $algo)
+    {
+        $this->algo = $algo;
+    }
+
     #[Route('/ping', name: 'app_home')]
     public function index(): JsonResponse
     {
+        return $this->json([
+            'message' => 'pong',
+        ]);
+    }
+
+    #[Route('/algo', name: 'app_algo')]
+    public function algo(): JsonResponse
+    {
+        $feno=$this->algo->add(56);
+        dd($feno);
         return $this->json([
             'message' => 'pong',
         ]);
